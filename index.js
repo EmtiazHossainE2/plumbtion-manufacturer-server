adaf709db54509eebd8af7b3531733b22d7b6aa0
EmtiazHossainE2 <emtiazemon310@gmail.com>
2022-05-23 01:53:46 +0600

    connect with mongodb and get tools data

--- index.js
+++ index.js
@@ -1,17 +1,52 @@
+//1
 const express = require('express');
-const app = express() 
-const port = process.env.PORT || 5000 
+const app = express()
+const port = process.env.PORT || 5000
 
+//3
 const cors = require('cors')
 require('dotenv').config()
 
+//4
 app.use(cors())
-app.use(express.json()) 
+app.use(express.json())
 
-app.get('/' , (req,res) => {
+//5 
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.emoo7.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+// console.log(uri);
+
+//6
+async function run() {
+    try {
+        //7
+        await client.connect();
+        const toolsCollection = client.db("plumbtion-manufacturer").collection("tools");
+
+        //8 get tool 
+        app.get('/tool' , async(req,res) => {
+            const query = {}
+            const tools = await toolsCollection.find(query).toArray()
+            res.send(tools)
+        })
+
+    }
+    finally {
+        //   await client.close();
+    }
+}
+run().catch(console.dir);
+
+
+
+//2
+app.get('/', (req, res) => {
     res.send('Plumbtion server is running')
 })
 
-app.listen(port , () => {
-    console.log('server running on ' , port);
+app.listen(port, () => {
+    console.log('server running on ', port);
 })
