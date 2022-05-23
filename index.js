68818433af409fa5399074aed94b203ece967ff7
EmtiazHossainE2 <emtiazemon310@gmail.com>
2022-05-23 23:52:38 +0600

    post review api

--- index.js
+++ index.js
@@ -195,11 +195,18 @@ async function run() {
 
         // ***    Review        **//
 
-        // get reviews 
+        //21 get reviews 
         app.get('/review', async (req, res) => {
             const query = {}
             const reviews = await reviewsCollection.find(query).toArray()
             res.send(reviews)
+        })
+
+        //22 post reviews
+        app.post('/review', async (req, res) => {
+            const review = req.body
+            const result = await reviewsCollection.insertOne(review)
+            res.send(result)
         })
 
     }
